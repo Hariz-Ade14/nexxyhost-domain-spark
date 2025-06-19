db6556a643dff44dd006eb6c2da3f02923cc24c0
Harizz_TheGreat <harizadebayo8@gmail.com>
2025-06-19 06:19:23 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -49,6 +49,19 @@ function ScrollToTop() {
   return null;
 }
 
+useEffect(() => {
+
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/5f0a41165b59f94722ba8cff/default';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
+
+},[]);
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
